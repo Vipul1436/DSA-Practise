4555493d04f49aba4c6858989eb60d09288e3f62
Vipul Singh <vipulsingh1436@gmail.com>
2026-06-20 09:45:09 +0530

    Added SortedArray

---
+++ Basics/SortedArray.java
@@ -0,0 +1,36 @@
+// Check if the Array is sorted or not
+import java.util.*;
+
+public class SortedArray{
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements in the array");
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array");
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int flag = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > arr[i+1]){
+                System.out.println("Array is not sorted");
+                flag = 1;
+                break;
+            }else{
+                flag = 0;
+            }
+        }
+        if(flag == 0){
+            System.out.println("Array is sorted");
+        }
+    }
+}
