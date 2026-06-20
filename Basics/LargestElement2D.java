11a8cbc3154ffcd187f92b91551faa6b69de0074
Vipul Singh <vipulsingh1436@gmail.com>
2026-06-20 13:29:08 +0530

    Added LargestElement2D array

---
+++ Basics/LargestElement2D.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Practise {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter no of rows");
+        int row = sc.nextInt();
+
+        System.out.println("Enter the no of columns");
+        int col = sc.nextInt();
+
+        int [][] arr = new int[row][col];
+
+        System.out.println("Enter the elements of the 2d Array");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                arr[i][j] = sc.nextInt(); 
+            }
+        }
+
+        int largest = arr[0][0];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if(arr[i][j] > largest){
+                    largest = arr[i][j];
+                }
+            }
+        }
+
+        System.out.println(largest);
+        
+    }
+}
--- Basics/SecondLargest.java
+++ Basics/SecondLargest.java
@@ -15,8 +15,8 @@ public class SecondLargest {
             arr[i] = sc.nextInt();
         }
 
-        int largest = arr[0];
-        int secondLargest = arr[1];
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
 
         for (int i = 0; i < arr.length; i++) {
             if(arr[i]> largest){
