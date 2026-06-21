e0c4dcd398ec21d97b21680d5e34b33dd8dd97fe
Vipul Singh <vipulsingh1436@gmail.com>
2026-06-21 14:15:55 +0530

    Added Richest Customer Wealth

---
+++ Arrays/TwoDArray/RichestCustomerWealth.java
@@ -0,0 +1,44 @@
+//
+
+import java.util.*;
+public class RichestCustomerWealth {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int row = sc.nextInt();
+
+        System.out.print("Enter the no of columns: ");
+        int col = sc.nextInt();
+
+        int[][] arr = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int ans = sum(arr);
+        System.out.println(ans);
+    }
+
+    static int sum(int[][] arr){
+        int maxSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            int sum = 0;
+            for (int j = 0; j < arr[i].length; j++) {
+                sum += arr[i][j]; 
+                if(sum > maxSum){
+                    maxSum = sum;
+                }  
+            }
+        }
+        return maxSum;
+    }
+    
+}
